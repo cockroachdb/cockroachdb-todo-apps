9156c0ac4db75a0d6c8fd75fc8aebd14ad0c12b5
homeusr <homeusr@homedesktop.net>
2020-10-04 22:04:25 -0400

    Update query to reflect db changes

--- clojure_todoapp_azetestuser1/sql/create.sql
+++ clojure_todoapp_azetestuser1/sql/create.sql
@@ -1,2 +1,3 @@
 create database todoapp;
-create table todoapp.item (id UUID NOT NULL DEFAULT gen_random_uuid() , description string, is_done boolean , update_dt time , constraint "primary" PRIMARY KEY (update_dt desc,id asc));
+
+create table todoapp.item (id UUID NOT NULL, description string, is_done boolean , update_dt timestamp , constraint "primary" PRIMARY KEY (id asc));
