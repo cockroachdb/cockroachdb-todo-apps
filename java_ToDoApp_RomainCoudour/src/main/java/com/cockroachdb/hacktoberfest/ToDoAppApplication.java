885defa6f323dbbd984be36250ff8be0612ddbd0
Romain <rom.coudour@gmail.com>
2020-10-10 12:24:10 +0200

    chore(): init spring app w/ spring init

---
+++ java_ToDoApp_RomainCoudour/src/main/java/com/cockroachdb/hacktoberfest/ToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.cockroachdb.hacktoberfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+}
---
+++ java_ToDoApp_RomainCoudour/src/test/java/com/cockroachdb/hacktoberfest/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cockroachdb.hacktoberfest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
