d60b75e23d1241a6c289a76048dbc331448e1264
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-10-11 21:23:34 +0800

    Implement initial functionality.

--- BASH_ToDoApp_jacklagare/run.sh
+++ BASH_ToDoApp_jacklagare/run.sh
@@ -14,3 +14,53 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+create(){
+    echo '== Create a new task =='
+    read -p 'Description: ' description
+    sqlStatement="INSERT INTO todo.tasks (description) VALUES ('$description');"
+    docker exec -it roach1 ./cockroach sql --insecure --execute="$sqlStatement"
+    
+    echo ''
+    echo ''
+}
+
+view(){
+    echo '== View a task =='
+    read -p 'Task ID: ' taskId
+    sqlStatement="SELECT * FROM todo.tasks WHERE id = $taskId;"
+    docker exec -it roach1 ./cockroach sql --insecure --execute="$sqlStatement"
+    
+    echo ''
+    echo ''
+}
+
+while [ "$choice" != '5' ]
+do
+    echo 'Hello! This is a Todo App using Bash and CockroachDb!'
+    echo 'To begin, please choose an action: '
+    echo '[1] Create a new task'
+    echo '[2] View task'
+    echo '[3] Delete task'
+    echo '[4] Show all tasks'
+    echo '[5] Exit'
+    echo
+    echo 'Enter number of your choice:'
+    read -p '> ' choice
+
+    case "$choice" in
+        "1")
+            create
+            ;;
+        "2")
+            view
+            ;;
+        "3")
+            echo '3'
+            ;;
+        "4")
+            echo '4'
+            ;;
+    esac
+done
+
+
