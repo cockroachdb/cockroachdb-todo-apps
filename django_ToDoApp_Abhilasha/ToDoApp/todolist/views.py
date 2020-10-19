f1bffb3d87f2231bf7a749d9c9be89389dc00ee8
Abhilasha06 <abhisinha662000@gmail.com>
2020-10-20 03:06:48 +0530

    copyright added

--- django_ToDoApp_Abhilasha/ToDoApp/todolist/views.py
+++ django_ToDoApp_Abhilasha/ToDoApp/todolist/views.py
@@ -1,7 +1,22 @@
+"""
+Copyright [2020] [Abhilasha Sinha]
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, 
+software distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and limitations under the License.
+"""
+
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
 from .models import Task
 from .forms import TaskForm
 # Create your views here.
+
 def new_task(request):
     if request.method == "POST":
         form = TaskForm(request.POST)
